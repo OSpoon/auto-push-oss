b728631ec0b13cbd5e132b5e7c90b8d796c68856
fe-xiaoxin <zxin088@gmail.com>
2022-09-10 10:09:32 +0800

    🎉 init:

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # auto-push-oss
+
 自动推动目录到 OSS
+
+## Inputs
+
+|参数|描述|
+|----|----|
+|`root`|待推送路径|
+|`bucket`|oss bucket|
+|`region`|oss region|
+|`accessKeyId`|oss accessKeyId|
+|`accessKeySecret`|oss accessKeySecret|
+
+## Example usage
+
+```yaml
+uses: OSpoon/auto-push-oss@master
+with:
+  root: ./public
+  bucket: it200
+  region: oss-cn-beijing
+  accessKeyId: ${{secrets.accessKeyId}}
+  accessKeySecret: ${{secrets.accessKeySecret}}
+```
---
+++ index.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const fs = require("fs");
+
+const core = require("@actions/core");
+const OSS = require("ali-oss");
+
+// 接收输入参数
+const root = core.getInput("root");
+const bucket = core.getInput("bucket");
+const region = core.getInput("region");
+const accessKeyId = core.getInput("accessKeyId");
+const accessKeySecret = core.getInput("accessKeySecret");
+
+const client = new OSS({
+  bucket,
+  region,
+  accessKeyId,
+  accessKeySecret,
+});
+
+const rootPath = path.resolve(__dirname, root || "./dist");
+
+let filepaths = [];
+let putCount = 0;
+
+function readFileSync(filepath) {
+  let files = fs.readdirSync(filepath);
+  files.forEach((filename) => {
+    let p = path.join(filepath, filename);
+    let stats = fs.statSync(p);
+    if (stats.isFile()) {
+      filepaths.push(p);
+    } else if (stats.isDirectory()) {
+      readFileSync(p);
+    }
+  });
+}
+
+function put(filepath) {
+  const p = filepath.replace(rootPath, "").substr(1);
+  return client.put(p.replace("\\", "/"), filepath);
+}
+
+async function update() {
+  try {
+    // 递归获取所有待上传文件路径
+    readFileSync(rootPath);
+    let retAll = await filepaths.map((filepath) => {
+      putCount++;
+      console.log(`任务添加: ${path.basename(filepath)}`);
+      return put(filepath);
+    });
+    Promise.all(retAll).then((res) => {
+      const resAll = res.map((r) => {
+        return r.res.statusCode === 200;
+      });
+      if (Object.keys(resAll).length === putCount) {
+        console.log("发布成功");
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// 上传发布
+update();
